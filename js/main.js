232f5b3e9ca9109da1a901c2f12de38406f75852
ErickDC <erickdiazc717@gmail.com>
2021-09-10 15:01:23 -0500

    solucionando lo de las flechas

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
-const fila = document.querySelector('.container-principal');
-const animepic = document.querySelector('.animespic');
+// Anime del día - Arrows
+var fila = document.querySelector('.container-carrusel');
+var animepic = document.querySelector('.animespic');
 
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
+var flechaIzquierda = document.getElementById('flecha-izquierda');
+var flechaDerecha = document.getElementById('flecha-derecha');
 
+flechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
+});
 
 flechaDerecha.addEventListener('click', () => {
     fila.scrollLeft += fila.offsetWidth;
 });
 
-flechaIzquierda.addEventListener('click', () => {
-    fila.scrollLeft -= fila.offsetWidth;
+// Anime de la semana - Arrows
+
+var fila2 = document.querySelector('.container-carrusel2');
+var animepic2 = document.querySelector('.animespic2');
+
+var flechaIzquierda2 = document.getElementById('flecha-izquierda2');
+var flechaDerecha2 = document.getElementById('flecha-derecha2');
+
+flechaIzquierda2.addEventListener('click', () => {
+    fila2.scrollLeft -= fila2.offsetWidth;
 });
+
+flechaDerecha2.addEventListener('click', () => {
+    fila2.scrollLeft += fila2.offsetWidth;
+});
+
